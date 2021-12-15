6c06f57e97b299c2f8aa3b2b2f164dbc58293d2a
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-12-15 16:54:57 +0100

    Inserite mail in modo dinamico

--- js/script.js
+++ js/script.js
@@ -7,14 +7,22 @@ const app = new Vue(
             mailArray: []
         },
         methods: {
+            
+        },
 
+        created: function() {
+            
             // devo fare una funzione che prende 10 mail dall'API e le 
             // inserisce all'interno di mailArray
+            for( let i = 0; i < 10; i++ ) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    const singleMail = (response.data.response);
+                    this.mailArray.push(singleMail);
+                });
+            }
             
         }
+
     }
 );
-
-
-// API per le mail
-// https://flynn.boolean.careers/exercises/api/random/mail
