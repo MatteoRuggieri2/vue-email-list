2e6b4bad0f642ae6a5b9d0830f6e31fb311ea0cb
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-12-15 15:53:12 +0100

    Creata struttura html

--- js/script.js
+++ js/script.js
@@ -4,10 +4,17 @@ const app = new Vue(
     {
         el: '#root',
         data: {
-            
+            mailArray: []
         },
         methods: {
+
+            // devo fare una funzione che prende 10 mail dall'API e le 
+            // inserisce all'interno di mailArray
             
         }
     }
 );
+
+
+// API per le mail
+// https://flynn.boolean.careers/exercises/api/random/mail
